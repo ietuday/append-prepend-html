8045a8b65ddf49abd6dff9e1ccbfc871a0d0c2e9
udayaditya singh <udayaditya.singh@gmail.com>
2020-10-03 19:19:43 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'append-prepend-html';
+  myHtml: string = '<div><i>basic initial HTML</i></div>';
+  appendedHtml: string = '<div><b>this appended html</b></div>';
+  prenpendHtml: string = '<div><b>this prepended html</b></div>';
+
+  append(): void {
+    this.myHtml = this.myHtml + this.appendedHtml;
+  }
+
+  prepend(): void {
+    this.myHtml = this.prenpendHtml + this.myHtml;
+  }
+
+  reset(): void{
+    this.myHtml = `<div><i>basic initial HTML</i></div>`;
+  }
 }
